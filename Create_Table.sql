3db84c3f338b7e883b872d27d785f49a602455ca
CryptoMaNa <76812251+ManishaNath@users.noreply.github.com>
2024-01-10 00:16:08 +0530

    Create Create_Table.sql

---
+++ Create_Table.sql
@@ -0,0 +1,50 @@
+##Movie Table
+
+# -- Create movie table
+CREATE TABLE Movie (
+movie_id INT NOT NULL AUTO_INCREMENT, 
+movie_name VARCHAR(50) NOT NULL, 
+total_time TIME, 
+Genres VARCHAR(50), 
+language VARCHAR(50), 
+movie_ratings VARCHAR(50), 
+PRIMARY KEY(movie_id)
+ );
+
+##Theater Table
+
+# -- Create Theatre table
+
+CREATE TABLE Theatre (
+    Theatre_ID VARCHAR(10) NOT NULL,
+    Theatre_name VARCHAR(255) NOT NULL,
+    Location VARCHAR(255) NOT NULL,
+    PRIMARY KEY(Theater_ID)
+);
+
+
+##Show Table
+
+# -- Create Show table
+CREATE TABLE Show (
+    Show_ID INT NOT NULL PRIMARY KEY,
+    Show_date VARCHAR(100),
+    Show_Time VARCHAR(50)
+);
+
+
+##Screen Table
+
+#-- Create Screen table
+CREATE TABLE Screen (
+    Screen_ID INT PRIMARY KEY,
+    Screen_name VARCHAR(50)
+);
+
+
+
+
+
+
+
+
