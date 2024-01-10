8aca100b9fc3f69a692902cb2cc7d03c02b23970
CryptoMaNa <76812251+ManishaNath@users.noreply.github.com>
2024-01-10 14:44:17 +0530

    Update BCNFP1,P2.sql

--- BCNF.sql
+++ BCNF.sql
@@ -82,3 +82,36 @@ VALUES
     (12, '2D', 2),
     (13, '2D', 3),
     (14, '4D', 4);
+
+-- View
+
+-- View all the table
+
+     select * from Movie
+     select * from Theater
+     select * from ShowTable
+     select * from MappingTable
+     select * from Screen
+
+-- Query
+
+ // P2 - Write a query to list down all the shows on a given date at a given theatre along with their respective show timings.
+
+
+SELECT
+    Movie.movie_name AS MovieName,
+    Theater.theater_name AS TheatreName, 
+    ShowTable.Show_date AS ShowDate,
+    ShowTable.Show_Time AS ShowTime
+FROM 
+    Show 
+INNER JOIN 
+    Movie_Show MS ON ShowTable.MT_ID = MS.Movie_ID
+INNER JOIN 
+    Movie_TheaterShow MTS ON MS.Movie_ID = MTS.Movie_ID
+INNER JOIN 
+    Theater T ON MTS.Theater_ID = T.Theater_ID
+INNER JOIN 
+    Movie M ON MS.Movie_ID = M.Movie_ID
+WHERE
+   ShowTable.Show_Date = '25.4.23';
